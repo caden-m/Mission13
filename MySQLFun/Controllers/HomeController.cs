7fe4f916beffab133ad93082bae824219eeb2c4f
caden-m <90535137+caden-m@users.noreply.github.com>
2022-03-30 23:37:32 -0600

    First

---
+++ MySQLFun/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MySQLFun.Models;
+
+namespace MySQLFun.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly ILogger<HomeController> _logger;
+
+        private BowlingLeagueDbContext _context { get; set; }
+
+        public HomeController(BowlingLeagueDbContext temp)
+        {
+            _context = temp;
+        }
+
+        public IActionResult Index()
+        {
+            var blah = _context.Bowlers.ToList();
+
+            return View(blah);
+        }
+
+        public IActionResult Privacy()
+        {
+            var blah = _context.Bowlers.ToList();
+
+            return View(blah);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MySQLFun/Models/BowlingLeagueDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MySQLFun.Models
+{
+    public class BowlingLeagueDbContext : DbContext
+    {
+        public BowlingLeagueDbContext(DbContextOptions<BowlingLeagueDbContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<Bowler> Bowlers { get; set; }
+    }
+}
